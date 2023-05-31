f1abeb15f55596c31227d9cd5edb3dc64c439453
frez7 <116892049+frez7@users.noreply.github.com>
2023-05-31 19:37:02 +0600

    Add Enrollments(#10)

---
+++ StudentHelper.Model/Models/Entities/CourseEntities/Enrollment.cs
@@ -0,0 +1,14 @@
+﻿using StudentHelper.Model.Models.Common;
+
+namespace StudentHelper.Model.Models.Entities.CourseEntities
+{
+    public class Enrollment : BaseEntity<int>
+    {
+        public int SellerId { get; set; }
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+        public int ReceivedMoney { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
---
+++ StudentHelper.WebApi/Migrations/Course/20230531133543_addEnrollments.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentHelper.WebApi.Migrations.Course
+{
+    /// <inheritdoc />
+    public partial class addEnrollments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Enrollments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SellerId = table.Column<int>(type: "int", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    ReceivedMoney = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enrollments", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Enrollments");
+        }
+    }
+}
