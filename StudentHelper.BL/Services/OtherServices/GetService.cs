6c94dc3a94b700a0fd3f889d075253f12064a733
frez7 <116892049+frez7@users.noreply.github.com>
2023-06-04 20:47:35 +0600

    adding validation service (#15)

---
+++ StudentHelper.BL/Services/OtherServices/GetService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace StudentHelper.BL.Services.OtherServices
+{
+    public class GetService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public GetService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public int GetCurrentUserId()
+        {
+            var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            int.TryParse(userId, out var id);
+
+            return id;
+        }
+    }
+}
