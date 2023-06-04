2ca0f510e2c8c7ba09f8540e50922f24b8b2d8c8
ataimamatov <altair.mamatov@gmail.com>
2023-06-04 20:33:16 +0600

    GetSellerById

---
+++ StudentHelper.Model/Models/Common/SellerResponses/SellerDTOResponse.cs
@@ -0,0 +1,18 @@
+﻿using StudentHelper.Model.Models.Entities.SellerEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentHelper.Model.Models.Common.SellerResponses
+{
+    public class SellerDTOResponse : Response
+    {
+        public SellerDTO? SellerDTO { get; set; }
+        public SellerDTOResponse(int statusCode, bool success, string message, SellerDTO sellerDTO) : base(statusCode, success, message)
+        {
+            SellerDTO = sellerDTO;
+        }
+    }
+}
